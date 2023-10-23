a6623fe800d039ac840227231379e9dc39dc1098
unknown <momohemmanuel370@gmail.com>
2023-10-23 16:45:52 +0100

    added models and made migrations

--- workspace/littlelemon/restaurant/models.py
+++ workspace/littlelemon/restaurant/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    inventory = models.IntegerField(
+        validators=[
+            MaxValueValidator(99999),  
+            MinValueValidator(0),  
+        ]
+        
+    )
+    def __str__(self) -> str:
+        return self.title
+    
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField(
+        validators=[
+            MaxValueValidator(999999),  
+            MinValueValidator(0),  
+        ]
+        )
+    booking_date = models.DateTimeField(auto_now_add=True)
+    def __str__(self) -> str:
+        return self.name
+    
+
